6dc0309b19d73cb9e591ef2c79b73092dcf21887
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2021-07-14 10:37:17 +0100

    Add favicon and return 503 for html

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from apig_wsgi import make_lambda_handler
-from bottle import default_app, template, static_file, route
+from bottle import default_app, template, static_file, route, response
 
 app = default_app()
 
 lambda_handler = make_lambda_handler(app)
+
+
+def service_unavailable():
+    response.status = 503
+    response.body = template('index.html')
+    return response
+
+
+@route('/favicon.ico')
+def favicon():
+    return static_file("favicon.ico", "./static")
 
 
 @route('/static/<path:path>')
@@ -13,9 +24,9 @@ def static(path):
 
 @route('/<path:re:.*>')
 def other_paths(path):
-    return template('index.html')
+    return service_unavailable()
 
 
 @route('/<path:re:.*>', method='POST')
 def other_paths(path):
-    return template('index.html')
+    return service_unavailable()
