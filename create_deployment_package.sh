f453acd2f3da609b2a9529347375c27b31c57d2b
Thanh <tri.lam21@outlook.com>
2024-01-05 10:20:13 +0000

    Allow deploy job to deploy different unavailable messages

--- create_deployment_package.sh
+++ create_deployment_package.sh
@@ -1,9 +1,25 @@
+if [ $# -eq 0 ]; then
+  echo "Please provide an argument (either 'index' or 'xmas')"
+  exit 1
+fi
+
 mkdir -p package
 pip3 install --target package -r requirements-runtime.txt
 cd package
 zip -r ../deployment.zip .
 cd ..
+
+if [ "$1" = "default" ]; then
+  cp views/default/index.html views/index.html
+  zip -g deployment.zip views/index.html
+elif [ "$1" = "xmas" ]; then
+  cp views/xmas/index.html views/index.html
+  zip -g deployment.zip views/index.html
+else
+  echo "Invalid argument. Please use 'index' or 'xmas'"
+  exit 1
+fi
+
 zip -g deployment.zip app.py
-zip -g deployment.zip views/index.html
 zip -g deployment.zip static/tna-horizontal-white-logo.svg
 zip -g deployment.zip static/main.css
